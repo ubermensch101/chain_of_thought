47c350158d911bbb3a7ff0e3a8ee7e902b017b90
ubermensch101 <76596256+ubermensch101@users.noreply.github.com>
2023-11-29 16:52:23 +0530

    Adding streamlit UI

--- temp.py
+++ temp.py
@@ -1,3 +1,21 @@
-from predict import *
+import streamlit as st
+from predict import predict_one  
 
-print(predict_one("What is 2+2?"))
+def main():
+    st.title("Math Word Problem Solver")
+    user_query = st.text_area("Enter a math word problem:")
+
+    if st.button("Submit"):
+        if user_query:
+            answer = predict_one(user_query)
+            st.subheader("Answer:")
+            formatted_answer = format_response(answer)
+            st.code(formatted_answer, language='python')
+
+def format_response(response):
+    answer, explanation = response
+    formatted_response = f"Answer: {answer}\n\nExplanation:\n{explanation}"
+    return formatted_response
+
+if __name__ == "__main__":
+    main()
